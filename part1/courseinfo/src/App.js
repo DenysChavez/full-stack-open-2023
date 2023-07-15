c65c1ce0a9982f4892f4da44eaa9fa73e85c5452
Denys <go4denys@gmail.com>
2023-07-15 18:44:52 -0400

    ini git in the root project

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,18 @@
+import Total from './components/Total';
+import Header from './components/Header';
+import Content from './components/Content';
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
