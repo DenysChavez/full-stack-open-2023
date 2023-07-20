a7a08b942804b591d55eee57c2dc4dbc836ba5de
Denys <go4denys@gmail.com>
2023-07-20 11:02:54 -0400

    finished step8 from part2

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,11 +1,14 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ parts }) => {
+
   return (
     <>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+          <Total exercises={parts}/>
     </>
   );
 };
