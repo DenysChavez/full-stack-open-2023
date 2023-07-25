c75b4a6102252320dceb080d43a1ef1e3ba185f5
Denys <go4denys@gmail.com>
2023-07-25 10:46:07 -0400

    sending data to the server

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -30,17 +30,20 @@ const App = (props) => {
 
   useEffect(hook, [])
 
+
   const addNote = (event) => {
     event.preventDefault();
 
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   };
 
   const hanldeNoteChange = (event) => {
