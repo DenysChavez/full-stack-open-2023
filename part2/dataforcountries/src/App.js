2bb7786a158616191a988a0709af8f92ee0bc6fc
Denys <go4denys@gmail.com>
2023-07-25 22:48:12 -0400

    finished exercises 2.18-2.20: dataforcountries

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import Search from './components/Search';
+import { useState, useEffect } from "react"
+import axios from "axios"
+import CountryList from "./components/CountryList"
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState('fi')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then(({ data }) => {
+      setCountries(data)
+    })
+  }, [])
+
+  const matchedContries = countries.filter(c => c.name.common.toLowerCase().includes(search.toLocaleLowerCase()))
+
   return (
-    <div className="App">
-      <Search />
+    <div>
+      <div>
+        find country <input value={search} onChange={({ target }) => setSearch(target.value)} />
+      </div>
+      <CountryList 
+        countries={matchedContries}
+        showCountry={setSearch}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- part2/dataforcountries/src/components/Country.js
+++ part2/dataforcountries/src/components/Country.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const Country = ({ country }) => {
+
+  const [weather, setWeather] = useState(null)
+  const languages = Object.values(country.languages)
+  const flagUrl = country.flags.png
+  const capital = country.capital[0]
+
+  useEffect(() => {
+    const API_KEY = process.env.REACT_APP_API_KEY
+    const lat = country.capitalInfo.latlng[0]
+    const lon = country.capitalInfo.latlng[1]
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+    axios.get(url).then(({ data }) => {
+      setWeather(data)
+    })
+  }, [])
+
+  if (!weather) {
+    return null
+  }
+
+  const icon = weather.weather[0].icon
+  const weatherIconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
   return (
     <div>
-      <h1>{country.name.common}</h1>
-      <div>Capital: {country.capital}</div>
-      <div>Area: {country.area} sq. km</div>
-      <img src={country.flags.png} alt={`flag of ${country.name.common}`} />
-      <h2>Languages:</h2>
+      <h2>{country.name.common}</h2>
+
+      <p>population {country.population}</p>
+      <p>capital {capital}</p>
+
+      <h4>languages</h4>
+
       <ul>
-        {Object.values(country.languages).map((language, index) => <li key={index}>{language}</li>)}
+        {languages.map(language => <li key={language}>{language}</li>)}  
       </ul>
+
+      <img src={flagUrl} width='200' />
+
+      <h4>Weather in {capital}</h4>
+
+      <p>temperature {weather.main.temp} Celsius</p>
+
+      <img src={weatherIconUrl} width='80' />
+
+      <p>wind {weather.wind.speed} m/s</p>
     </div>
   )
 };
